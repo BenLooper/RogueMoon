2634929dca5c8022dc0e8cbbb3ec9bf867494f71
BenLooper <benlooper08@gmail.com>
2020-11-21 15:25:54 -0600

    Added jwt auth, userInfo gets set in store w/token

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :cards
   resources :games
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/auth', to: 'auth#create'
 end
