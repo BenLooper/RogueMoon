bd30570fc70565a4c18c97c03f8aa06344df7cb2
BenLooper <benlooper08@gmail.com>
2020-12-01 14:15:06 -0600

    Created home screen, added bot decisions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,18 +11,19 @@ import { useEffect } from 'react'
 import history from './history'
 
 import Login from './components/Login.js'
+import Home from './containers/Home'
 import GameBoard from './containers/GameBoard'
 
 
 const App = () => {
-
+  let gameOn = useSelector((state) => state.gameOn)
   let dispatch = useDispatch()
   //check for token in localStorage
   //if there is one, fetch it in useEffect()
   //if not, redirect to login
   useEffect(() => {
-    if (window.localStorage.token) {
-      fetch('http://localhost:3000/auth',{
+    if (window.localStorage.token && !(gameOn)) {
+      fetch('http://localhost:3000/auth', {
         method: 'GET',
         headers: {
           "Content-Type": "application/json",
@@ -30,11 +31,26 @@ const App = () => {
           "Authorization": `Bearer ${window.localStorage.token}`
         }
       })
-      .then(res=>res.json())
-      .then(userInfo => {
-        dispatch({type: 'SET_USER', user:userInfo.user, ownedCards:userInfo.cards, games:userInfo.games})
-        history.push('/')
+        .then(res => res.json())
+        .then(userInfo => {
+          dispatch({ type: 'SET_USER', user: userInfo.user, ownedCards: userInfo.cards, games: userInfo.games })
+          history.push('/')
+        })
+    }
+    else if (window.localStorage.token && gameOn) {
+      fetch('http://localhost:3000/auth', {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": `Bearer ${window.localStorage.token}`
+        }
       })
+        .then(res => res.json())
+        .then(userInfo => {
+          dispatch({ type: 'SET_USER', user: userInfo.user, ownedCards: userInfo.cards, games: userInfo.games })
+          history.push('/game')
+        })
     }
     else {
       history.push('/login')
@@ -45,8 +61,8 @@ const App = () => {
     <Router history={history}>
       <Provider store={store}>
         <Route exact path='/login' component={Login} />
-        {/* Root is gameboard atm, this will change when I add campaign board */}
-        <Route exact path='/' component={GameBoard} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/game' component={GameBoard} />
       </Provider>
     </Router >
   );
